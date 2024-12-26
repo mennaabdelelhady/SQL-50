74436bed7518408e28f0445149f5cbd48fe27625
mennaabdelelhady <menamahmod660@gmail.com>
2024-12-26 23:48:06 +0200

    left-join&coalesce

---
+++ Basic Joins/1934. Confirmation Rate.sql
@@ -0,0 +1,9 @@
+SELECT s.user_id,ROUND(
+    COALESCE(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) * 1.0 / COUNT(c.user_id), 0),
+        2
+    ) AS confirmation_rate
+FROM signups s
+LEFT JOIN confirmations c
+ON s.user_id=c.user_id
+GROUP BY 
+    s.user_id;
